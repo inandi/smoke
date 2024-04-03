53740dbbcb2aa5898be36c01023df5215187b59f
Gobinda Nandi <gobinda_nandi@icloud.com>
2024-04-03 14:13:23 +0530

    add banner

--- app/src/main/java/com/inandi/smoke/MainActivity.kt
+++ app/src/main/java/com/inandi/smoke/MainActivity.kt
@@ -11,21 +11,28 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.inandi.smoke.ui.theme.SmokeTheme
+import android.widget.TextView
+//import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            SmokeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main) // Ensure this matches your layout file's name.
+        val topLabel = findViewById<TextView>(R.id.topLabel)
+        // Example: Change text programmatically
+        topLabel.text = "Everyday you are wining!!!"
+
+//        setContent {
+//            SmokeTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    Greeting("Android")
+//                }
+//            }
+//        }
     }
 }
 
