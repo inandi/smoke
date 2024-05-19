51d1d2fdd28979d55ffe3bc758c70a357800de3c
Gobinda Nandi <gobinda_nandi@icloud.com>
2024-05-19 14:06:14 +0530

    color upgrade

--- app/src/main/java/com/inandi/smoke/QuitSmokingProgressAdapter.kt
+++ app/src/main/java/com/inandi/smoke/QuitSmokingProgressAdapter.kt
@@ -26,6 +26,12 @@ class QuitSmokingProgressAdapter(private val progressData: Array<Array<String>>)
         holder.milestoneTextView.text = milestone[1]
         holder.awardTextView.text = milestone[2]
         holder.descriptionTextView.text = milestone[3]
+
+        // Set text color to white
+        holder.milestoneTextView.setTextColor(holder.itemView.resources.getColor(android.R.color.white))
+        holder.awardTextView.setTextColor(holder.itemView.resources.getColor(android.R.color.white))
+        holder.descriptionTextView.setTextColor(holder.itemView.resources.getColor(android.R.color.white))
+
     }
 
     override fun getItemCount(): Int {
